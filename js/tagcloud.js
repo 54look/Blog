5ae09dde6706addb10f524670143140fd5dea49c
54look <251962672@qq.com>
2020-01-23 01:49:51 +0800

    Site updated: 2020-01-23 01:49:51

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -13,9 +13,9 @@ addLoadEvent(function() {
    try {
        TagCanvas.textFont = 'Trebuchet MS, Helvetica';
        TagCanvas.textColour = '#333';
-       TagCanvas.textHeight = 25;
+       TagCanvas.textHeight = 15;
        TagCanvas.outlineColour = '#E2E1D1';
-       TagCanvas.maxSpeed = 0.2;
+       TagCanvas.maxSpeed = 0.1;
        TagCanvas.outlineMethod = 'block';
        TagCanvas.minBrightness = 0.2;
        TagCanvas.depth = 0.92;
